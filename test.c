b9553fbe24d735b8719eeb9d23b4c773d45dc05c
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2024-12-15 16:56:45 +0100

    add test file

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// diese funktion sortiert ein array mit der schnellen sortiermethode
+int quicksort(int *arr, int left, int right)
+{
+    return 0;
+}
+
+/*
+ * Hello World program in C.
+ * Line 2
+ * Line 3
+ */
+int main()
+{
+    for (int i = 0; i < 10; ++i) {
+        printf("Hello World: %d\n", i);
+    }
+    printf("\n");
+    return 0;
+}
